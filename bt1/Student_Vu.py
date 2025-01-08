1a3ab41e950188b57982711adbd7718fa599f180
vuhayhat <142613355+vuhayhat@users.noreply.github.com>
2025-01-08 09:16:01 +0700

    Thêm mã nguồn trong thư mục bt1

---
+++ bt1/Student_Vu.py
@@ -0,0 +1,11 @@
+from Student import Student
+
+class Long(Student):
+    def name(self):
+        return "Vu"
+    def speak(self):
+        return "Toi len la: Vu.\nGmail: vuhayhatmtp@gmail.com"
+    def address(self):
+        return "Dong Hoi-Quang Binh"
+    def telegram_id(self):
+        return "7719282102"
