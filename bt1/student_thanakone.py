890c95216fcdfd1d0b5a6fd7eadf5f5fca042d1d
Thanakone <thanakone7536@gmail.com>
2025-01-08 16:08:16 +0700

    Add files via upload

---
+++ bt1/student_thanakone.py
@@ -0,0 +1,11 @@
+from Student import Student
+
+class Thanakone(Student):
+    def name(self):
+        return "THANAKONE"
+    def speak(self):
+        return "Toi Ten la: THANAKONE.\nGmail: thanakone7536@gmail.com"
+    def address(self):
+        return "Savannakhet - Laos"
+    def telegram_id(self):
+        return "888746846"
