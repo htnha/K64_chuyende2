fc76cc794f8d7b4275df81f23cb1b8e12e988bf7
htnha <hoang.tuan.nha@gmail.com>
2025-01-08 15:16:38 +0700

    New student

---
+++ bt1/Student.py
@@ -0,0 +1,9 @@
+class Student:
+    def speak(self):
+        raise NotImplementedError("Subclass must implement abstract method")
+
+    def name(self):
+        raise NotImplementedError("Subclass must implement abstract method")
+
+    def address(self):
+        raise NotImplementedError("Subclass must implement abstract method")
