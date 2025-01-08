e89fad2e17a49ad594a4fa9a92dd67d6f38e1756
htnha <hoang.tuan.nha@gmail.com>
2025-01-08 15:22:30 +0700

    New student

--- bt1/main1.py
+++ bt1/main1.py
@@ -1,4 +1,3 @@
-from student_Nha import Nha
 from student_Long import Long
 from student_Dung import Dung
 from student_Ekalat import Ekalat
@@ -8,7 +7,7 @@ from student_Phong import Phong
 from student_Dat import Dat
 
 # Create a list of students
-k64_class = [Nha(), Long(), Dung(), Ekalat(), Tham(), Linh(), Phong(), Dat()]
+k64_class = [Long(), Dung(), Ekalat(), Tham(), Linh(), Phong(), Dat()]
 
 for student in k64_class:
     try:
