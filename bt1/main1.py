89a919ee967aea0f290e4a9d13a47dd9f18f70a8
Mai Vu Bao Long <109711387+MVBL112004@users.noreply.github.com>
2025-01-08 15:20:06 +0700

    long

--- bt1/main1.py
+++ bt1/main1.py
@@ -1,1 +1,19 @@
+from student_Nha import Nha
+from student_Long import Long
+from student_Dung import Dung
+from student_Ekalat import Ekalat
+from student_Tham import Tham
+from student_Linh import Linh
+from student_Phong import Phong
+from student_Dat import Dat
 
+# Create a list of students
+k64_class = [Nha(), Long(), Dung(), Ekalat(), Tham(), Linh(), Phong(), Dat()]
+
+for student in k64_class:
+    try:
+        print(student.name())
+        print(student.speak())
+        print(student.address())
+    except NotImplementedError as e:
+        print(f"Co loi cho sinh vien: {type(student).__name__} - {e}")
---
+++ bt1/student_Long.py
@@ -0,0 +1,9 @@
+from Student import Student
+
+class Long(Student):
+    def name(self):
+        return "Mai Vu Bao Long"
+    def speak(self):
+        return "Toi len la: Long.\nGmail: longbao112004@gmail.com"
+    def address(self):
+        return "Dong Hoi-Quang Binh"
