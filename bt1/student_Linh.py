216a21e2da1a6d01c35e94977178921c451a0215
dinhcaolinh2004 <dinhcaolinh2004@gmail.com>
2025-01-08 15:32:41 +0700

    Linh.py

---
+++ bt1/student_Linh.py
@@ -0,0 +1,9 @@
+from Student import Student
+
+class Linh(Student):
+    def name(self):
+        return "Dinh Cao Linh"
+    def speak(self):
+        return "Toi len la: Linh.\nGmail: dinhcaolinh2004@gmail.com"
+    def address(self):
+        return "Dong Hoi-Quang Binh"
