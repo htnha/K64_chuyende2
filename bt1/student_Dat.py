3be8e5bf187a98d1d2de8bf35678478b2b4651f0
ten cua ban <149994716+baodat946@users.noreply.github.com>
2025-01-08 15:31:14 +0700

    update

---
+++ bt1/student_Dat.py
@@ -0,0 +1,9 @@
+from Student import Student
+
+class Dat(Student):
+    def name(self):
+        return "Dat"
+    def speak(self):
+        return "Toi len la: Dat \nEmail: phamnguyenbaodat946@gmail.com"
+    def address(self):
+        return "DH city"
