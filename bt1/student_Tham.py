c7eab0e3cf13fab4191e23c17ea16112f3a32495
nguyenvantham12345 <160579622+nguyenvantham12345@users.noreply.github.com>
2025-01-08 15:24:37 +0700

    Update student_Tham.py

--- bt1/student_Tham.py
+++ bt1/student_Tham.py
@@ -6,4 +6,4 @@ class Tham(Student):
     def speak(self):
         return "Toi len la: Tham \nEmail: nguyenvantham.vtabd@gmail.com"
     def address(self):
-        return "Quang Trach country"
+        return "Quang Trach - Quang Binh"
