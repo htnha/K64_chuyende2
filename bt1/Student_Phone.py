b4c6c9474137b95dee28b871024bc9dab7461084
Phonepasert <tradtrarist@gmail.com>
2025-01-08 16:02:34 +0700

    Add files via upload

---
+++ bt1/Student_Phone.py
@@ -0,0 +1,9 @@
+from Student import Student
+
+class Phone(Student):
+    def name(self):
+        return "Phone"
+    def speak(self):
+        return "Toi len la: Phone \nEmail: tradtrarist@gmail.com"
+    def address(self):
+        return "DH city"
