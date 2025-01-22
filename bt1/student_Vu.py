09e0d2ef8a9d0d4400925bb0644b4b2447119da5
vuhayhat <vuhayhatmtp@gmail.com>
2025-01-22 16:10:44 +0700

    update

---
+++ bt1/student_Vu.py
@@ -0,0 +1,13 @@
+from Student import Student
+
+class Long(Student):
+    def name(self):
+        return "Vu"
+    def speak(self):
+        return "Toi len la: vu.\nGmail: vuhayhatmtp@gmail.com"
+    def address(self):
+        return "Dong Hoi-Quang Binh"
+    def telegram_id(self):
+        return ""
+    def ip(self):
+        return "20.0.0.23"
